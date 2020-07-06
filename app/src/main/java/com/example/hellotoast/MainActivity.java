0a5f421713eb13ead8701cbea36325cb8eacf0eb
user <yeost-wm19@student.tarc.edu.my>
2020-07-06 15:01:34 +0800

    Hello Constraint

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    Button zeroColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +32,29 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+
+        if(mCount % 2 == 0)
+        {
+            view.setBackgroundColor(Color.MAGENTA);
+        }
+        else
+            view.setBackgroundColor(Color.GREEN);
+
+        if(mCount != 0)
+        {
+            zeroColor = (Button) findViewById(R.id.button_zero);
+            zeroColor.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    public void resetZero(View view)
+    {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+
+        if(mCount == 0)
+            view.setBackgroundColor(Color.DKGRAY);
+
+
     }
 }
